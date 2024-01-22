f134b409fa320b4c7db41da453612f786d61b33f
Alexey <info@createit.ru>
2024-01-22 11:05:48 +0300

    Fixed installer bug with FileManTemplate policy

--- _build/resolvers/policy.php
+++ _build/resolvers/policy.php
@@ -1,31 +1,33 @@
 <?php
 
-/** @var xPDO\Transport\xPDOTransport $transport */
-/** @var array $options */
-
 use MODX\Revolution\modAccessPolicy;
 use MODX\Revolution\modAccessPolicyTemplate;
+use xPDO\Transport\xPDOTransport;
+use xPDO\xPDO;
 
+
+/** @var xPDO\Transport\xPDOTransport $transport */
 if ($transport->xpdo) {
     $modx = $transport->xpdo;
+    /** @var array $options */
     switch ($options[xPDOTransport::PACKAGE_ACTION]) {
         case xPDOTransport::ACTION_INSTALL:
         case xPDOTransport::ACTION_UPGRADE:
             // Assign policy to template
-            $policy = $modx->getObject(modAccessPolicy::class, array('name' => 'FileManUserPolicy'));
+            $policy = $modx->getObject(modAccessPolicy::class, array('name' => 'FileManPolicy'));
             if ($policy) {
-                $template = $modx->getObject(modAccessPolicyTemplate::class, ['name' => 'FileManUserPolicyTemplate']);
+                $template = $modx->getObject(modAccessPolicyTemplate::class, ['name' => 'FileManPolicyTemplate']);
                 if ($template) {
                     $policy->set('template', $template->get('id'));
                     $policy->save();
                 } else {
                     $modx->log(
                         xPDO::LOG_LEVEL_ERROR,
-                        '[FileMan] Could not find FileManUserPolicyTemplate Access Policy Template!'
+                        '[FileMan] Could not find FileManPolicyTemplate Access Policy Template!'
                     );
                 }
             } else {
-                $modx->log(xPDO::LOG_LEVEL_ERROR, '[FileMan] Could not find FileManUserPolicyTemplate Access Policy!');
+                $modx->log(xPDO::LOG_LEVEL_ERROR, '[FileMan] Could not find FileManPolicyTemplate Access Policy!');
             }
             break;
     }
